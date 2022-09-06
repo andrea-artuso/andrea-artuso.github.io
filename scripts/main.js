f6fcd05cbd8c2afed82b5d5c87f32ca94120a42b
andrea-artuso <ultrandr3x@gmail.com>
2022-09-06 22:52:32 +0200

    Removed blank lines

--- scripts/main.js
+++ scripts/main.js
@@ -4,5 +4,4 @@ function sendMail(){
     if (confirm("The email address has been copied!\nDo you want to send an email directly?")){
         window.open("mailto:andrea.artuso.business@gmail.com", '_blank');
     }
-
 }
