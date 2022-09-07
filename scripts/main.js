957eddbe95f11c75862399d1a1cbe0ccf1d4526f
andrea-artuso <ultrandr3x@gmail.com>
2022-09-07 12:09:44 +0200

    Added footer with dynamic year

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
+// Catching DOM
+const footerYear = document.getElementById('footerYear');
+
+
+// Get current year
+let year = new Date().getFullYear();
+
+
+// add current year in footer
+footerYear.innerText = year;
+
+// Functions
 function sendMail(){
     navigator.clipboard.writeText("andrea.artuso.business@gmail.com");
 
