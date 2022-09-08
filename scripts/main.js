656cf3164f99ba5a1e2be45a8d11d49cd27be308
andrea-artuso <ultrandr3x@gmail.com>
2022-09-08 23:18:02 +0200

    Created resume section

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,33 @@
 // Catching DOM
 const footerYear = document.getElementById('footerYear');
+const skills = document.querySelectorAll('.skill');
+
+
+// Constants
+const skillsArr = Array.from(skills);
+const descriptions = [
+    'html5',
+    'css3',
+    'js',
+    'reactjs',
+    'php',
+    'mysql',
+    'nodejs',
+    'postman',
+    'c',
+    'arduino',
+    'debian',
+]
+
+
+// Onload
+window.onload = () => {
+    skills.forEach((skill) => {
+        skill.addEventListener('click', function(){
+            alert(descriptions[skillsArr.indexOf(skill)]);
+        })
+    })
+}
 
 
 // Get current year
@@ -16,4 +44,11 @@ function sendMail(){
     if (confirm("The email address has been copied!\nDo you want to send an email directly?")){
         window.open("mailto:andrea.artuso.business@gmail.com", '_blank');
     }
+}
+
+
+function downloadCv(){
+    let path = './static/assets/';
+
+    window.open(path, '_blank');
 }
