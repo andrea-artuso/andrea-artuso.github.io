95c1f7c493dd9dfdd17b3b2112e56a7603933127
andrea-artuso <ultrandr3x@gmail.com>
2022-11-12 18:50:53 +0100

    Created landing page

---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+const API_URL = 'https://programming-quotes-api.herokuapp.com/quotes/random';
+const TERMINAL_TEXT =  "\'name\': \'Andrea Artuso\',\n\'country\': \'Italy\',\n\'date-of-birth\': \'22-04-2005\',\n\'languages\': [\'italian\', \'english\'],\n\'programming-languages\': [\'C/C++\', \'C#\', \'JavaScript\', \'Python\'],\n\'other-technologies\': [\'HTML\', \'CSS\', \'Linux\'],\n\'contacts\': {\nㅤ\t\t\'email-address\': \'andrea.artuso.business@gmail.com\',\nㅤ\t\t\'linkedin\': \'linked.com/in/andrea-artuso\',\nㅤ\t\'github\': \'github.com/andrea-artuso\'\nㅤ\t\t}"
+const WINDOW_HEIGHT = window.innerHeight;
+
+// catching the DOM
+const quote_content = document.getElementById('quote_content');
+const quote_author = document.getElementById('quote_author');
+const terminal_text = document.getElementById('terminal_text');
+const landing = document.getElementById('landing');
+
+window.onload = () => {
+    getRandomQuote();
+    displayCode();
+}
+
+// Get random quote function
+function getRandomQuote(){
+    fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
+        quote_content.innerHTML = `<span class='quotation'>“</span>${data.en}<span class='quotation'>”</span>`;
+        quote_author.innerHTML = `~ ${data.author}`;
+    })
+}
+
+// Display code
+function displayCode(){
+    let codes = TERMINAL_TEXT.split('\n');
+    for (let i=0; i<codes.length; i++){
+        (function(ind) {
+            setTimeout(() => {
+                terminal_text.innerHTML += `<div><span class="tilde">~</span> ${codes[i]} </div>`;
+            }, (500 * ind));
+        })(i);
+    }
+}
+
+// blur landing on scroll
+window.addEventListener('scroll', () => {
+    let scrollY = window.scrollY;
+    let blur_perc = (25 * scrollY) / WINDOW_HEIGHT;
+
+    if (blur_perc > 100) blur_perc = 100;
+    landing.style.filter = "blur("+ blur_perc +"px)";
+
+    console.log(blur_perc);
+})
